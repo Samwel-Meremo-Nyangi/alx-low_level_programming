9b72e63290c559a5b25feeea20e3321f31db4acc
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-16 13:15:23 -0800

    Printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - the printf function
- * return 0 (Success)
+ * main - A printf program to print a line
+ * return: 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
