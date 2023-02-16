d1db9925fc2eb31e18f65a8439aacbaa1d0f40b1
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-16 14:15:05 -0800

    Sizes of data types and computer types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  A program to print the sizes of data types
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char z;
+	int y;
+	long int x;
+	long long int w;
+	float v;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(z));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
+printf("Size of a long long int: byte(s)\n", (unsigned long)sizeof(w));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(v));
+return (0);
+}
