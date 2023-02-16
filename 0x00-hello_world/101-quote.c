0f56c17b7cb76cf67d0dd9024dcad1fa5c30dcc2
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-16 15:02:05 -0800

    Standard error Quote Task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program in C printing an output to standard error
- * No using ma (3) printf and puts
- * Return 1 (Success)
+ * main - A program in C that prints a line to the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 25-10-19\n", stdout);
 return (1);
 }
