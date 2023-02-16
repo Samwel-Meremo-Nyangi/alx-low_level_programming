63b282014ccbdee5fc2523516773a8ae8e638e5e
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-16 12:41:12 -0800

    Hello, puts!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-
+/**
+ * main - This is the entry to a program and its function
+ * puts - prints the set of characters
+ * return 0 - (Success)
+ */
 int main(void)
 {
-	puts("\Programming is like building a multilingual puzzle");
-	
-	return (0);
-
+puts("\"Programming is building a multilingual puzzle");
+return (0);
 }
