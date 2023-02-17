91de603e8f5802b4b262af48e3b665e70d684572
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-17 11:34:32 -0800

    lowercase and then UPPERCASE alphABETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry of main function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char q;
+	char w;
+
+	q = 'a';
+	w = 'A';
+	while
+		(q <= 'z') {
+			putchar(q);
+			q++;
+		}
+	while
+		(w <= 'Z') {
+			putchar(w);
+			w++;
+		}
+	putchar('\n');
+	return (0);
+}
