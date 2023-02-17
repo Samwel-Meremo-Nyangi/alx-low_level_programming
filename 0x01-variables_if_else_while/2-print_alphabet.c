0d69fbd811880c69bd290a106a08e85893b684b1
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-17 11:13:54 -0800

    Printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry of program here
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char s;
+
+	s = 'a';
+	while
+		(s <= 'z'){
+			putchar(s);
+			s++;
+		}
+	putchar('\n');
+	return (0);
+}
