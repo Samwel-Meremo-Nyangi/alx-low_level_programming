6133211516ef3e77e9b64122845546911e0be6f8
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-17 11:51:11 -0800

    Printing looping numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Start of program where code starts to execute
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int v;
+
+	for (v = 0; v < 10; v++)
+		printf("%d", v);
+	putchar('\n');
+	return (0);
+}
