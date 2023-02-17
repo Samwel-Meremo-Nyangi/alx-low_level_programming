5857fe8475e65c2f237c6a0fff5e737a1f3a0e67
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-17 12:14:28 -0800

    Numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program starts here at entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'a') {
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
