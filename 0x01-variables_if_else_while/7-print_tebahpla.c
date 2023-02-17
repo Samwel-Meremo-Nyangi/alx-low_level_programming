cb94f85686c9c492f16d8c5d60f69268409b7ec6
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-17 12:55:36 -0800

    Printing alphabets reversely

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry of program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char u;
+
+	u = 'z';
+	while
+		(u >= 'a') {
+			putchar(u);
+			u--;
+		}
+	putchar('\n');
+	return (0);
+}
