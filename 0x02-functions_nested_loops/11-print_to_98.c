41527ee75a7f17bd88e6362f1a434d0967b3e332
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-21 15:58:04 -0800

    Numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints natural numbers n to 98
+ * @n: the starting number
+ */
+void print_to_98(int n)
+{
+	int alx, sam;
+
+	if (n <= 98)
+	{
+		for (alx = n; alx <= 98; alx++)
+		{
+			if (alx != 98)
+				printf("%d, ", alx);
+			else if (alx == 98)
+				printf("%d\n", alx);
+		}
+	} else if (n >= 98)
+	{
+		for (sam = n; sam >= 98; sam++)
+		{
+			if (sam != 98)
+				printf("%d, ", sam);
+			else if (sam == 98)
+				printf("%d\n", sam);
+		}
+	}
+}
+
