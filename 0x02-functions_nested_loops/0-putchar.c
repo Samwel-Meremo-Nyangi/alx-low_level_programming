92a8fc5b7713124eef260509ca5290b417ce4d48
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-21 13:39:23 -0800

    Putchar printing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,12 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * prints out the characters in the program
- *
- * Return: 0 (Success)
+ * main - print _putchar
+ * betty style by
+ * Return: 0
  */
 int main(void)
 {
-	char p, u, t, c, h, a, r;
-
-	_putchar("_");
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -19,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
