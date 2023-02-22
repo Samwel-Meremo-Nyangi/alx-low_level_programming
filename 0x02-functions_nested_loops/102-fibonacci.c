d7d445f1c85030834d72379484730460f68a10b5
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-21 17:10:11 -0800

    program to print the first 100 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -Main program entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int k = 0;
+	long int s = 0, h = 1, next;
+
+	while (k < 50)
+	{
+		next = s + h;
+		s = h;
+		h = next;
+		printf("%lu", next);
+
+		if (k < 49)
+		{
+			printf(". ");
+		}
+		k++;
+	}
+	putchar('\n);
+	return(0);
+}
