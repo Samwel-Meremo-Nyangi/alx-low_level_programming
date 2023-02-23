f28938d318823ebf295a4940587021f94abfea0a
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-23 14:33:35 -0800

    Print A Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ *
+ * @size: size of the triangle
+ *
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int base, height;
 
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (height = 1; height <= size; height++)
+		{
+			for (base = 1; base <= size; base++)
+			{
+				if ((height + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
