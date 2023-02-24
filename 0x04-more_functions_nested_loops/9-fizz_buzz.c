1ca459519a4085e52f85b40640eb80c3326d4cd6
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-24 12:59:43 -0800

    Fizz Buzz Fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,20 @@ int main(void)
 {
 	int num;
 
-	for (num = 1; num <= 100; num++)
+	num = 1;
+	for (num = 2; num <= 100; num++)
 	{
-		if (num % 3 == 0 && num % 5 != 0)
+		if (num % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if (num % 5 == 0 && num % 3 != 0)
+		else if (num % 5 == 0)
 		{
 			printf("Buzz");
 		}
 		else if (num % 3 == 0 && num % 5 == 0)
 		{
 			printf("FizzBuzz");
-		}
-		else if (num == 1)
-		{
-			printf("%d", num);
 		}
 		else
 		{
