61da19aeabea108902bc678bd4d118c225d9baea
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-02-27 14:54:59 -0800

    Copy and Paste stings in destination

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,17 @@
 #include "main.h"
-
 /**
  * _strcpy - copies the string
- *
  * @dest: copy destination
  * @src: source integer
  * Return: char
  */
-
 char *_strcpy(char *dest, char *src)
 {
 	int m;
 
 	for (m = 0; src[m] != '\0'; m++)
 	{
-		*(dest + m) = *(src + m);;
+		*(dest + m) = *(src + m);
 	}
 	*(dest + m) = '\0';
 	return (dest);
