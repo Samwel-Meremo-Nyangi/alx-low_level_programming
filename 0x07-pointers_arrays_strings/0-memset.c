04587516c6883cea3def51ce765614481a6a0b82
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-03-06 00:39:43 -0800

    Filling memory buffer

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - filling memory blocks
+ * @s: memory to be filled
+ * @b: value
+ * @n: bytes to be changed
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int indexset = 0;
+
+	for (; n > 0; indexset++)
+	{
+		s[indexset] = b;
+		n--;
+	}
+	return (s);
+}
