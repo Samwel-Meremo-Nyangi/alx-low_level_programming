a5c6c2541578c07fac65099a36ce1ce422a843fd
Samwel Meremo <samwelnyangi@zetech.ac.ke>
2023-03-10 12:07:28 -0800

    0x0A. C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argc: number of args
  * Return: int
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
